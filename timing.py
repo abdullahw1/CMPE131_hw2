ccca89cd7d87430f123f6bc24f7a6d41b26dae45
abdullahw1 <90009221+abdullahw1@users.noreply.github.com>
2021-10-14 16:50:22 -0700

    Update timing.py

--- timing.py
+++ timing.py
@@ -1,10 +1,12 @@
 import time
 
-def calculate_time():
-    #store start time
-    start = time.time()
-    time.sleep(2)
-    # store end time
-    end = time.time()
-    print("total", end - start)
+def calculate_time(method):
+    def timer():
+        #store start time
+        start = time.time()
+        time.sleep(2)
+        # store end time
+        end = time.time()
+        print("total", end - start)
+       
     return timer
