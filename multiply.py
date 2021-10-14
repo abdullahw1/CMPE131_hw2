7a87e4c038fe8e5cddc0c3510b93515086157559
abdullahw1 <90009221+abdullahw1@users.noreply.github.com>
2021-10-14 14:55:22 -0700

    Update multiply.py
    commit2

--- multiply.py
+++ multiply.py
@@ -1,1 +1,9 @@
+def multiply_list(list):
+    product = 1
+    for x in list:
+        if type(x)==int:
+            product = product * x
+        else:
+            return False
 
+    return product
