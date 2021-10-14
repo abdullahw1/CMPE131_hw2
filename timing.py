af671cfab2b69293aaaf6ecf0ffdc4f75157b5aa
abdullahw1 <90009221+abdullahw1@users.noreply.github.com>
2021-10-14 16:46:20 -0700

    Update timing.py
    commit2

--- timing.py
+++ timing.py
@@ -1,1 +1,11 @@
+import time
+
+def calculate_time():
+    #store start time
+    start = time.time()
+    time.sleep(2)
+    # store end time
+    end = time.time()
+    print("total", end - start)
+calculate_time()
 
