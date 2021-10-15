bb5fb61f1bb24f7e3a6909987fd399fdffe09b7c
abdullahw1 <90009221+abdullahw1@users.noreply.github.com>
2021-10-14 20:06:23 -0700

    Update triple.py

--- triple.py
+++ triple.py
@@ -1,1 +1,20 @@
+def tripler(function):
+    """
+        This functon will use  decorator to call
+        function to call given function 3 times.
 
+        Parameters
+        ----------
+        function : function
+            runs in decorator
+    """
+    def decorator():
+        function()
+        function()
+        function()
+    return decorator
+@tripler
+
+def test():
+    print("test")
+test()
