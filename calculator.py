135ee5b62bea5db20ee0d330909de513b2b431d3
abdullahw1 <90009221+abdullahw1@users.noreply.github.com>
2021-10-14 17:18:02 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,43 +1,40 @@
-def calculator(number1,number2,operator):
-    # "+" operator will perform addition
-    if operator=="+":
-        print(number1+number2)
-    # else if "-" operator used, then subtraction performed
-    elif operator=="-":
-        print(number1-number2)
-    # else if "*" operator used, multiplication will be performed
-    elif operator=="*":
-        print(number1*number2)
-    # else if "/" operator is used, division performed
-    elif operator=="/":
-        print(number1/number2)
-    # else if operator is "//",then perform integer division and print result
-    elif operator=="//":
+def calculator(number1, number2, operator):
+    """
+        This function will allow to make calculations
+    """
+    #"+" operator will perform addition
+    if operator == "+":
+        print(number1 + number2)
+    #else if "-" operator used, then subtraction performed
+    elif operator == "-":
+        print(number1 - number2)
+    #else if "*" operator used, multiplication will be performed
+    elif operator == "*":
+        print(number1 * number2)
+    #else if "/" operator is used, division performed
+    elif operator == "/":
         #return false if num2 is 0 as result wont exist
-        if number2 == 0:
-            return False
-        else:
-            print(number1//number2)
-
+        print(number1 / number2)
+    #perform integral division with "//" operator
+    elif operator == "//":
+        print(number1 // number2)
     # else perform power operation with "**" operator
-    elif operator=="**":
-        print(number1**number2)
-    # else return False
-    else:
-        return False
+    elif operator == "**":
+        print(number1 ** number2)
+        
+  def parse_input():
+    """
+    get the text of user input and parse the text.
+    This function splits user input into number1, number2, and operator.
+    passes variables to calculator() function.
+    """
+    num1 =  float (input("Enter 1st number: "))
+	num2 = float (input ("Enter 2nd number: "))
+	operator = input ("Operator: ")
+    # pass user input to calculator() function
+	calculator(num1, num2, operator)
 
-# input_output() function
-def parse_input():
-    number1 = 0.0
-    number2 = 0.0
-    print("Enter equation: ")
-    text = input()
-    # split text into three variables by " ", So that we can get the data
-    strList = text.split(" ")
-    # prevent system get error when user input wrong text
-    try:
-        number1 = float(strList[0])
-        number2 = float(strList[2])
-    except ValueError:
-        return False
-    calculator(number1, number2, strList[1])
+parse_input()
+
+    
+
